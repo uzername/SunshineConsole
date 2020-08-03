6b0a11c3619606e17f7b32756bd5e2b79f4cba6a
uzername <ivanpost777@gmail.com>
2020-08-03 09:47:45 +0300

    Added SunshineConsoleDemo

---
+++ SunshineConsoleDemo/SunshineConsoleDemo/MainClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SunshineConsole;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace SunshineConsoleDemo
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            ConsoleWindow console = new ConsoleWindow(16, 40, "Sunshine Console");
+            console.Write(6, 14, "Sunshine Console", Color4.White);
+            while (!console.KeyPressed && console.WindowUpdate())
+            {
+
+            }
+        }
+    }
+}
